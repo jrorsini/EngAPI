5cfa787fcbf4174f4ba91bc846281ffc2620f48d
Jean-Roger <jeanroger.orsini@gmail.com>
2018-03-22 02:22:30 +0900

    Minor.

---
+++ app.js
@@ -0,0 +1,16 @@
+const formidable = require('formidable')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors())
+app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/upload', (req, res) => {
+  const form = new formidable.IncomingForm();
+
+  form.parse(req, (err, fields, files) => {
+    console.log(files.filetoupload);
+    res.send(files.filetoupload.name)
+  })
+})
+app.listen(3000, () => console.log('Up & Running...'))
