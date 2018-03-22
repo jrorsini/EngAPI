e532144529d4d980747a2c36c4627643b89819e8
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-03-22 09:19:23 +0900

    Can now save files.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const formidable = require('formidable');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const fs = require('fs');
 
 app.use(cors());
 app.get('/', (req, res) => res.send('Hello World!'));
@@ -9,7 +10,13 @@ app.post('/upload', (req, res) => {
 	const form = new formidable.IncomingForm();
 
 	form.parse(req, (err, fields, files) => {
-		res.send(files.filetoupload);
+		const oldpath = files.filetoupload.path;
+		const newpath = 'videos/' + files.filetoupload.name;
+		fs.rename(oldpath, newpath, function(err) {
+			if (err) throw err;
+			res.write('File uploaded and moved!');
+			res.end('File uploaded');
+		});
 	});
 });
 app.listen(3000, () => console.log('Up & Running...'));
