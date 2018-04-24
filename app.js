22cbf0df0c3a347cab065dedeb71dc6da8ea72fd
Jean-Roger <jeanroger.orsini@gmail.com>
2018-04-24 23:24:41 +0900

    Minor.

--- app.js
+++ app.js
@@ -11,30 +11,33 @@ const irr = require('./irr-verbs');
 
 app.use(cors());
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/add', (req, res) => {
+	console.log(req.body);
+});
+
 app.post('/upload', (req, res) => {
 	const form = new formidable.IncomingForm();
 
 	// form.on('progress', function(bytesReceived, bytesExpected) {
 	// 	console.log(Math.round(bytesReceived / bytesExpected * 100) + '%');
 	// });
-  form.maxFileSize = 2000 * 1024 * 1024;
+	form.maxFileSize = 2000 * 1024 * 1024;
 
 	form.parse(req, (err, fields, files) => {
-    if(err) throw err
+		if (err) throw err;
 
 		const oldpath = files.filetoupload.path;
-    const newpath = 'videos/' + files.filetoupload.name;
+		const newpath = 'videos/' + files.filetoupload.name;
 
-    fs.readdir('videos/', (err, files) => {
-
-      if(err) throw err;
-      if(files.length) fs.unlinkSync(`videos/${files[0]}`);
-      fs.rename(oldpath, newpath, function(err) {
-
-        if (err) throw err;
-        res.send(`/${__dirname}/${newpath}`);
-      });
-    })
+		fs.readdir('videos/', (err, files) => {
+			if (err) throw err;
+			if (files.length) fs.unlinkSync(`videos/${files[0]}`);
+			fs.rename(oldpath, newpath, function(err) {
+				if (err) throw err;
+				res.send(`/${__dirname}/${newpath}`);
+			});
+		});
 	});
 });
 app.listen(3000, () => console.log('Up & Running...'));
