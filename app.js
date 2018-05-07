0b4666c9211770989c47534c47f8b64e05847972
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-05-07 16:55:43 +0900

    Added glosbe

--- app.js
+++ app.js
@@ -1,43 +1,57 @@
-const formidable = require('formidable');
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const fs = require('fs');
-const irr = require('./irr-verbs');
+const formidable = require('formidable')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const fs = require('fs')
+const request = require('request')
+const irr = require('./irr-verbs')
 
 // console.log(irr.find('hello, today I ate some pasta, and ran off.'));
 // console.log(irr.find('He wound up being arrested.'));
 // console.log(irr.find("I didn't,"));
 
-app.use(cors());
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(cors())
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/search/:word', (req, res) => {
+	request(
+		`https://glosbe.com/gapi/translate?from=eng&dest=fra&format=json&phrase=test`,
+		function(error, response, body) {
+			if (!error && response.statusCode === 200) {
+				res.send(body)
+			} else {
+				res.send(error)
+			}
+		}
+	)
+})
 
 app.post('/add', (req, res) => {
-	console.log(req.body);
-});
+	console.log(req.body)
+})
 
 app.post('/upload', (req, res) => {
-	const form = new formidable.IncomingForm();
+	const form = new formidable.IncomingForm()
 
 	// form.on('progress', function(bytesReceived, bytesExpected) {
 	// 	console.log(Math.round(bytesReceived / bytesExpected * 100) + '%');
 	// });
-	form.maxFileSize = 2000 * 1024 * 1024;
+	form.maxFileSize = 2000 * 1024 * 1024
 
 	form.parse(req, (err, fields, files) => {
-		if (err) throw err;
+		if (err) throw err
 
-		const oldpath = files.filetoupload.path;
-		const newpath = 'videos/' + files.filetoupload.name;
+		const oldpath = files.filetoupload.path
+		const newpath = 'videos/' + files.filetoupload.name
 
 		fs.readdir('videos/', (err, files) => {
-			if (err) throw err;
-			if (files.length) fs.unlinkSync(`videos/${files[0]}`);
+			if (err) throw err
+			if (files.length) fs.unlinkSync(`videos/${files[0]}`)
 			fs.rename(oldpath, newpath, function(err) {
-				if (err) throw err;
-				res.send(`/${__dirname}/${newpath}`);
-			});
-		});
-	});
-});
-app.listen(3000, () => console.log('Up & Running...'));
+				if (err) throw err
+				res.send(`/${__dirname}/${newpath}`)
+			})
+		})
+	})
+})
+app.listen(9000, () => console.log('Up & Running...'))
